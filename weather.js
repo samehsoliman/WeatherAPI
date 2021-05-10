254bc6443680825bca2da6284f0921e9d46ed1be
samehsoliman <50579022+samehsoliman@users.noreply.github.com>
2021-05-10 16:32:20 -0400

    Add files via upload

--- weather.js
+++ weather.js
@@ -3,6 +3,7 @@ class Weather {
         this.appId = '25d6df8753ec8f3b0f5a68f9f339a0be';
     }
 
+    // make http call to weather api
     async getWeather(loc){
         const resp = await fetch(`https://api.openweathermap.org/data/2.5/find?q=${loc.city},${loc.country}&units=metric&appid=${this.appId}`);
         const respData = await resp.json();
@@ -10,67 +11,3 @@ class Weather {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Weather {
-//     constructor(city, country){
-//         this.city = city;
-//         this.country = country;
-//         this.appId = '25d6df8753ec8f3b0f5a68f9f339a0be';
-//     }
-
-//     // get weather api
-//     async getWeather() {
-//         const resp = await fetch (`https://api.openweathermap.org/data/2.5/find?q=${this.city},${this.country}&units=metric&appid=${this.appId}`);
-
-//         const respData = await resp.json();
-//         return respData;
-//     }
-
-//     // change city
-//     changeCity(city, country){
-//         this.city = city;
-//         this.country = country;
-//     }
-
-// }
